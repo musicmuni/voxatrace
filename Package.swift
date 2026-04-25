88000f96975b323edb3e40d223558c6620e1207c
Gopala Krishna Koduri <gopala.koduri@gmail.com>
2026-04-25 22:56:46 +0530

    Release 1.0.1

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
 // swift-tools-version:5.9
 import PackageDescription
 
-let version = "1.0.0"
-let releaseTag = "voxatrace-v1.0.0"
-let checksum = "e5b17ea5af233c5526968b9e712e7059b1dea2c6b790c7f54f8d5140962aaec0"
+let version = "1.0.1"
+let releaseTag = "voxatrace-v1.0.1"
+let checksum = "4774b460e1ee9826366621cbcc543310985e72b8a9c1ab47e5d105ffcaee6c6d"
 
 let package = Package(
     name: "VoxaTrace",
